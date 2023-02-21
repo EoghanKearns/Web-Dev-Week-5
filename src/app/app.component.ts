ff1eae74c30f74d555898e29fc05bf4257617858
EoghanKearns <S00221854@mail.itsligo.ie>
2023-02-21 16:42:44 +0000

    modifed app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MovieService } from './movie.service';
+import { Movie } from 'src/movie.model';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +7,9 @@ import { MovieService } from './movie.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //title = 'movie-list';
-  movies=[] as any ;
+  public mySelectedMovie: Movie | undefined;
 
-  constructor(private movieService: MovieService) { }
-
-  ngOnInit() {
-    this.movies = this.movieService.getMovies();
-    console.log(this.movies);
+  setSelectedMovie(movie:Movie) {
+    this.mySelectedMovie = movie;
   }
-  // moviesList = [
-  //   { id: 1, title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola'},
-  //   { id: 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers'},
-  //   { id: 3, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock'},
-  //   { id: 4, title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}
-  // ];
 }
